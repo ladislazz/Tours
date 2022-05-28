19a839659021956d67586e3fab86b7b26afe80f1
Vlad Shalandin <vladislav.shalandin@gmail.com>
2022-05-28 11:44:22 -0400

    Set up states. Add basic conditional renders

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Tours from './Tours';
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project';
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <main>
       <section>
