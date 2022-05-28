2666f476ffed5436d3466b84e56222c9445f68e5
Vlad Shalandin <vladislav.shalandin@gmail.com>
2022-05-28 11:54:50 -0400

    Set up data fetching from the url (async fetchTours, useEffect)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
 import Tours from './Tours';
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project';
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setIsLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  console.log(tours);
 
   if (isLoading) {
     return <Loading />;
